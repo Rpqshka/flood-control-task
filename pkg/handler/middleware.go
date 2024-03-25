132dc43556907e3e1d0c614848b5f9c106c1d764
Aleksandr Golikov <rpqshka@gmail.com>
2024-03-25 20:33:44 +0300

    add negative args checker

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -1,15 +1,26 @@
 package handler
 
 import (
+	"errors"
 	"strconv"
+	"strings"
 )
 
 func checkParam(n, k string) (int64, int64, error) {
 	if n == "" {
 		n = "1"
 	}
+
 	if k == "" {
 		k = "5"
+	}
+
+	if strings.Contains(n, "-") {
+		return 0, 0, errors.New("invalid param n")
+	}
+
+	if strings.Contains(k, "-") {
+		return 0, 0, errors.New("invalid param k")
 	}
 
 	nInt, err := strconv.ParseInt(n, 10, 64)
@@ -21,5 +32,6 @@ func checkParam(n, k string) (int64, int64, error) {
 	if err != nil {
 		return 0, 0, err
 	}
+
 	return nInt, kInt, nil
 }
